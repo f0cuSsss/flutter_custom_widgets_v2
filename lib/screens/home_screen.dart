bcf2df1c1d3cbf37792a1d17ac4f0c43fd1035fa
f0cuS <f0cuSmcsza@gmail.com>
2021-10-01 13:02:30 +0300

    init commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: const [],
+          ),
+        ),
+      ),
+    );
+  }
+}
